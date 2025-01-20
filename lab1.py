57418f8d3efbc674935c07b683aad1bfbc8023ad
Nurbek <khodjabergen@gmail.com>
2025-01-20 05:53:57 +0500

    new commit3

--- lab1.py
+++ lab1.py
@@ -5,4 +5,31 @@ print("Hello World!")
 if 5 > 2:
     print("Five is greater than two!") 
 if 5 > 2:
-    print("Five is greater than two!")
+        print("Five is greater than two!")
+
+#Variables
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x, y, z)
+
+#Multiple Values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+a = b = c = "Orange"
+print(a)
+print(b)
+print(c)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
