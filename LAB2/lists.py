a700c360a745498d375d042edf763b123ca1557b
Nurbek <khodjabergen@gmail.com>
2025-01-27 19:50:47 +0500

    HW

---
+++ LAB2/lists.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+
+print(thislist[1]) #returns : apple
+print(thislist[-1]) #returns : cherry
+print(thislist[2:4]) #returns : ['cherry', 'apple']
+print(thislist[:4]) #returns : ['apple', 'banana', 'cherry', 'apple']
+print(thislist[2:]) #returns : ['cherry', 'apple', 'cherry']
