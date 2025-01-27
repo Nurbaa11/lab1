ec5269b78457f744fb95b3225e502fd66b6acf5c
Nurbek <khodjabergen@gmail.com>
2025-01-27 19:53:26 +0500

    HW

---
+++ LAB2/change_list_items.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist1 = ["apple", "banana", "cherry"]
+thislist1[1:2] = ["blackcurrant", "watermelon"]
+print(thislist1)
+
+thislist2 = ["apple", "banana", "cherry"]
+thislist2[1:3] = ["watermelon"]
+print(thislist2)
